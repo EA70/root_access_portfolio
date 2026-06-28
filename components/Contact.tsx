da27e9e020cb04e6a7e0ed4dc7b32d32c8059707
EA70 <eliekoyabanda@gmail.com>
2026-06-28 23:06:34 +0200

    variant problem

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -24,8 +24,7 @@ export default function Contact() {
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.6,
-        ease: "easeOut"
+        duration: 0.6
       }
     }
   };
